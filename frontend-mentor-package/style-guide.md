19e54baebaac9144b4363739e5c9e4073b6e51eb
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2023-09-15 05:18:25 +0300

    Project Add

--- frontend-mentor-package/style-guide.md
+++ frontend-mentor-package/style-guide.md
@@ -11,15 +11,6 @@ The designs were created to the following widths:
 
 ### Primary
 
-- Light Cyan: hsl(193, 38%, 86%)
-- Neon Green: hsl(150, 100%, 66%)
-
-### Neutral
-
-- Grayish Blue: hsl(217, 19%, 38%)
-- Dark Grayish Blue: hsl(217, 19%, 24%)
-- Dark Blue: hsl(218, 23%, 16%)
-
 ## Typography
 
 ### Body Copy
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import buttonImg from "./images/icon-dice.svg";
+import divider from "./images/pattern-divider-desktop.svg";
+import divider1 from "./images/pattern-divider-mobile.svg";
+import "./styles.scss";
+
 function App() {
-  return <div>App</div>;
+  const [adviceNumber, setAdviceNumber] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    setAdvice(response.data.slip.advice);
+    setAdviceNumber(response.data.slip.id);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [count]);
+
+  return (
+    <div className="card">
+      <p className="adviceNumber">
+        ADVICE <span>#{adviceNumber}</span>
+      </p>
+
+      <p className="advice">“{advice}”</p>
+
+      <div className="divider"></div>
+
+      <div onClick={handleClick} className="adviceChanger">
+        <img src={buttonImg} alt="diceImg" />
+      </div>
+    </div>
+  );
 }
 export default App;
